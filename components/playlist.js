a6792a7ba7535ae5e6e31941278ca9d9b4c452d0
Justin Thomas <jcthomas94@Justins-MacBook-Air.local>
2022-01-24 17:51:09 -0800

    modularize playlist component

--- components/playlist.js
+++ components/playlist.js
@@ -3,18 +3,33 @@ import { useSelector } from "react-redux"
 import { selectPlaylistArtists } from "../redux/slices/music.slice"
 import { getUniqueID } from "../utils/getUniqueID"
 
+function PlaylistModal(props){
+
+    const playlistArtists = useSelector(selectPlaylistArtists)
+    const onGenerateClick = () => console.log('generating playlist...')
+
+    return(
+        <> 
+            {playlistArtists.map(artist => <li key={getUniqueID()}>{artist}</li>)}
+            <button onClick={onGenerateClick}>Generate</button>
+            <br />
+            <button onClick={props.flipOpen}>Close</button> 
+        </>
+    )
+}
+
+function PlaylistButton(props){
+    return(
+        <button onClick={props.flipOpen}>Playlist</button> 
+    )
+}
+
 export default function Playlist(){
 
     const [open, setOpen] = useState(false)
-    const playlistArtists = useSelector(selectPlaylistArtists)
     const flipOpen = () => setOpen(!open)
 
     return (
-        !open ? 
-            <button onClick={flipOpen}>Playlist</button> 
-            :   <> 
-                    {playlistArtists.map(artist => <li key={getUniqueID()}>{artist}</li>)}
-                    <button onClick={flipOpen}>Close</button> 
-                </>
+        !open ? <PlaylistButton flipOpen={flipOpen}/> : <PlaylistModal flipOpen={flipOpen} />
     )
 }
